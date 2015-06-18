232754ab7c78860a2bb61130bdc8f0839dddfc83
Alan O'Donnell <alan.m.odonnell@gmail.com>
2015-06-18 16:37:32 -0400

    Spice up the example.

--- client/client.js
+++ client/client.js
@@ -5,26 +5,43 @@ import Promise from 'bluebird';
 const sleep = Promise.delay;
 
 function listen(el, type) {
-  const ch = pogo.chan();
+  const ch = pogo.chan(1);
   el.addEventListener(type, e => ch.putAsync(e));
   return ch;
 }
 
+let tickCount = 0;
+
+const clicks = listen(document, 'click');
+let clickCount = 0;
+
+const moves = listen(document, 'mousemove');
+let moveCount = 0;
+
 $(() => {
-  const clicks = listen(document, 'click');
+  const msgEl = document.getElementById('msg');
+
   pogo(function*() {
-    const msgEl = document.getElementById('msg');
-    let timeSinceLastClick = 0;
     while (true) {
-      const r = yield alts([clicks, sleep(1000)]);
+      const r = yield alts([sleep(1000), clicks, moves]);
       if (r === undefined) {
-        timeSinceLastClick += 1;
-        msgEl.innerHTML = timeSinceLastClick;
-      }
-      else {
-        timeSinceLastClick = 0;
-        msgEl.innerHTML = r.value.screenX + " " + r.value.screenY;
+        tickCount += 1;
+        msgEl.innerHTML = "ticks: " + tickCount;
+      } else if (r.channel === clicks) {
+        clickCount += 1;
+        msgEl.innerHTML = "clicks: " + clickCount;
+      } else {
+        moveCount += 1;
+        msgEl.innerHTML = "moves: " + moveCount;
       }
     }
   }).then(v => console.log("v", v), e => console.log("e", e));
+
+  pogo(function*() {
+    while (true) {
+      console.log("I'm running too :)");
+      yield sleep(1000);
+    }
+  });
+
 });
